4152d72f722a451be16c89c952f1621c6e825361
Jeff Apczynski <jeff.apczynski@shield.ai>
2026-01-02 02:34:39 -0500

    add print function

--- lib/ast.ml
+++ lib/ast.ml
@@ -3,3 +3,15 @@ type t =
   | Float of float
   | Integer of int
   | List of t list
+
+let rec print = function
+  | Symbol str -> str
+  | Float num -> string_of_float num
+  | Integer num -> string_of_int num
+  | List exprs ->
+    let str =
+      exprs
+      |> List.map print
+      |> String.concat " "
+    in
+    String.concat "" ["("; str; ")"]
