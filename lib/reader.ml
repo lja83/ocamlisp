8a8b3b94c916c9c8b0ea373373287eb9d18e0bf2
Jeff Apczynski <jeff.apczynski@shield.ai>
2026-01-02 02:34:12 -0500

    make sure expressions are properly delimited

--- lib/reader.ml
+++ lib/reader.ml
@@ -22,30 +22,33 @@ let dot = make_parser (fun x -> x = '.')
 let symbol =
   let* _ = peek alpha in
   let* sym = string_of (many1 (alpha <|> digit)) in
+  let* _ = peek delimiter in
   return (Ast.Symbol sym)
 
 let float =
   let float_of = map_opt float_of_string_opt in
   let* num = float_of (many1 (digit <|> dot)) in
+  let* _ = peek delimiter in
   return (Ast.Float num)
 
 let integer =
   let int_of = map_opt int_of_string_opt in
-  let* num = int_of (many1 digit) in
+  let* num = int_of (many1 (digit)) in
+  let* _ = peek delimiter in
   return (Ast.Integer num)
 
 let rec list s =
   (let* _ = open_paren in
    let* atoms = many sexp in
    let* _ = close_paren in
+   let* _ = peek delimiter in
    return (Ast.List atoms))
   s
 and sexp s =
   (let* _ = many whitespace in
-   let* exp = float <|> integer <|> symbol <|> list in
+   let* exp = integer <|> float <|> symbol <|> list in
    let* _ = many whitespace in
    return exp)
   s
 
-let read =
-  parse_string (many1 sexp)
+let read = parse_string sexp
