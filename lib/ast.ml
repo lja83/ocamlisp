60d14f36779cb34cec5bc0cae34c7447f8754e29
Jeff Apczynski <jeff.apczynski@shield.ai>
2026-01-03 00:47:23 -0500

    start working on eval

--- lib/ast.ml
+++ lib/ast.ml
@@ -15,3 +15,26 @@ let rec print = function
       |> String.concat " "
     in
     String.concat "" ["("; str; ")"]
+
+type number =
+  | Float of float
+  | Int of int
+
+module StringMap = Map.Make(String)
+let funcs = StringMap.of_seq @@ List.to_seq [
+    ("+", fun (x, y) -> Integer (x + y))
+]
+
+(*
+let rec eval = function
+  | Integer num -> Integer num
+  | Float num -> Float num
+  | Symbol str -> Symbol str
+  | List exprs ->
+    match List.map eval exprs with
+    | Some func :: args -> 
+      let func = StringMap.find_opt func funcs in
+      func args
+    | _ -> (Symbol "error")
+*)
+
